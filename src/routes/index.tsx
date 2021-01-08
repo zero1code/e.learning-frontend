a2de986845380f2ffb32b36d604f83ead0c9773a
Airton Oliveira <airtonoliveira4@hotmail.com>
2021-01-08 13:18:40 -0300

    feat: create Splash screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import Home from '../pages/Home';
+import Splash from '../pages/Splash';
 
 const App = createStackNavigator();
 
 const Routes: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  setTimeout(function () {
+    setLoading(true);
+  }, 2000);
+
+  if (loading) {
+    return (
+      <App.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <App.Screen name="Home" component={Home} />
+      </App.Navigator>
+    );
+  }
+
   return (
     <App.Navigator
       screenOptions={{
         headerShown: false,
       }}>
-      <App.Screen name="Home" component={Home} />
+      <App.Screen name="Splash" component={Splash} />
     </App.Navigator>
   );
 };
